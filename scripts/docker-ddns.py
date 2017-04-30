45c857a59b32ac1ed39fe2669102867265415b2f
Yury Stankevich <urykhy@gmail.com>
2017-04-30 12:46:43 +0300

    read docker events and update dns

--- scripts/docker-ddns.py
+++ scripts/docker-ddns.py
@@ -11,6 +11,7 @@
 
 import docker
 import re
+import json
 from collections import namedtuple
 
 import dns.query
@@ -83,11 +84,49 @@ def _update_ns(name, addr):
     if response.rcode() != 0:
         print "Failed: %s" % response
 
+def _rm_ns(name, addr):
+    print "delete from forward zone %s:%s ..." % (name, addr)
+    update = dns.update.Update("docker", keyring=KEYRING, keyalgorithm=ALGORITHM)
+    update.delete(name, "A")
+    response = dns.query.tcp(update, DDNS_SERVER)
+    if response.rcode() != 0:
+        print "Failed: %s" % response
+
+    rv = str(addr).split(".")
+    rv.reverse()
+    parts = ".".join(rv[:2])
+    zone_name = ".".join(rv[2:])+".in-addr.arpa"
+    print "delete from rev zone %s for %s:%s ..." % (zone_name, name, addr)
+    update = dns.update.Update(zone_name, keyring=KEYRING, keyalgorithm=ALGORITHM)
+    update.delete(parts, "PTR")
+    response = dns.query.tcp(update, DDNS_SERVER)
+    if response.rcode() != 0:
+        print "Failed: %s" % response
+
 events = client.events()
 for container in client.containers():
     for rec in _inspect(container['Id']):
         if rec.running:
             for addr in rec.addrs:
                 _update_ns(rec.name, addr)
+
+for raw in events:
+    evt = json.loads(raw)
+    if evt.get('Type', 'container') == 'container':
+        cid = evt.get('id')
+        if cid is None:
+            continue
+        status = evt.get('status')
+        if status == 'start':
+            for rec in _inspect(cid):
+                for addr in rec.addrs:
+                    for addr in rec.addrs:
+                        _update_ns(rec.name, addr)
+        if status == 'kill':
+            for rec in _inspect(cid):
+                for addr in rec.addrs:
+                    for addr in rec.addrs:
+                        _rm_ns(rec.name, addr)
+
 
 
