6884ee0349370d91573ad62bdc7dca732158aa3d
Yury Stankevich <urykhy@gmail.com>
2018-08-17 21:10:08 +0300

    bit of cctz

---
+++ time/Time.hpp
@@ -0,0 +1,104 @@
+#include <civil_time.h>
+#include <time_zone.h>
+
+#include <array>
+#include <chrono>
+#include <string>
+
+namespace Time
+{
+    using SC = std::chrono::system_clock;
+
+    inline cctz::time_zone load(const std::string& aName)
+    {
+        cctz::time_zone sZone;
+        if (!cctz::load_time_zone(aName, &sZone))
+            throw "fail to load zone";
+        return sZone;
+    }
+
+    enum fmt
+    {
+        DATE,
+        TIME,
+        DATETIME,
+        RFC1123,
+        ISO,
+        FORMAT_MAX
+    };
+
+    inline const std::string& formatString(fmt aFormat)
+    {
+        static std::array<std::string, FORMAT_MAX> sDict {
+            "%E4Y-%m-%d",
+            "%H:%M:%S",
+            "%E4Y-%m-%d %H:%M:%S",
+            "%a, %d %b %E4Y %H:%M:%S %Z",
+            "%E4Y%m%d%H%M%S",
+        };
+        if (aFormat >= FORMAT_MAX)
+            throw "formatString";
+        return sDict[aFormat];
+    }
+
+    class Zone
+    {
+        const cctz::time_zone m_Zone;
+    public:
+        Zone(const cctz::time_zone aZone) : m_Zone(aZone) { }
+
+        time_t parse(const std::string aValue, fmt aFormat) const
+        {
+            SC::time_point sPoint;
+            if (!cctz::parse(formatString(aFormat), aValue, m_Zone, &sPoint))
+                return -1;
+            return SC::to_time_t(sPoint);
+        }
+
+        std::string format(time_t sPoint, fmt aFormat) const
+        {
+            return cctz::format(formatString(aFormat), SC::from_time_t(sPoint), m_Zone);
+        }
+
+        std::string format(const cctz::civil_second& aTime, fmt aFormat) const
+        {
+            return format(to_unix(aTime), aFormat);
+        }
+
+        cctz::civil_second to_time(time_t sPoint) const
+        {
+            return cctz::convert(SC::from_time_t(sPoint), m_Zone);
+        }
+
+        cctz::civil_day to_date(time_t sPoint) const
+        {
+            return cctz::civil_day{cctz::convert(SC::from_time_t(sPoint), m_Zone)};
+        }
+
+        time_t to_unix(const cctz::civil_second& aTime) const
+        {
+            return SC::to_time_t(cctz::convert(aTime, m_Zone));
+        }
+    };
+
+    class Period
+    {
+        const Zone& m_Zone;
+        const unsigned m_Period;
+    public:
+        Period (const Zone& aZone, unsigned aPeriod) : m_Zone(aZone), m_Period(aPeriod) {}
+
+        cctz::civil_second round(const cctz::civil_second& aTime) const
+        {
+            auto sUnix = m_Zone.to_unix(aTime);
+            sUnix -= sUnix % m_Period;
+            return m_Zone.to_time(sUnix);
+        }
+
+        unsigned serial(const cctz::civil_second& aTime) const
+        {
+            auto sUnix = m_Zone.to_unix(aTime);
+            return sUnix / m_Period;
+        }
+    };
+}
---
+++ time/test.cpp
@@ -0,0 +1,35 @@
+/*
+ * small classes to wrap cctz library
+ *
+ * g++ test.cpp -I. -lboost_unit_test_framework -lcctz -I/usr/include/cctz
+ */
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE Suites
+#include <boost/test/unit_test.hpp>
+
+#include <Time.hpp>
+
+BOOST_AUTO_TEST_SUITE(Time)
+BOOST_AUTO_TEST_CASE(simple)
+{
+    Time::Zone t(Time::load("Europe/Moscow"));
+    BOOST_CHECK_EQUAL(t.parse("1970-01-01 01:02:03", Time::DATETIME), -7077);
+    BOOST_CHECK_EQUAL(t.parse("1970-01-01 03:00:00", Time::DATETIME), 0);
+    BOOST_CHECK_EQUAL(t.to_time(-7077), cctz::civil_second(1970,01,01,1,2,3));
+    BOOST_CHECK_EQUAL(t.to_unix(cctz::civil_second(1970,01,01,1,2,3)), -7077);
+    BOOST_CHECK_EQUAL(t.to_date(-7077), cctz::civil_day(1970,01,01));
+    BOOST_CHECK_EQUAL(t.format(-7077, Time::DATETIME), "1970-01-01 01:02:03");
+    BOOST_CHECK_EQUAL(t.format(-7077, Time::ISO), "19700101010203");
+    BOOST_CHECK_EQUAL(t.format(t.parse("1970-01-01 01:02:03", Time::DATETIME), Time::DATETIME), "1970-01-01 01:02:03");
+    BOOST_CHECK_EQUAL(t.parse("Sun, 06 Nov 1994 08:49:37 GMT", Time::RFC1123), 784100977);
+}
+BOOST_AUTO_TEST_CASE(period)
+{
+    Time::Zone t(cctz::utc_time_zone());
+    Time::Period p(t, 600);
+
+    BOOST_CHECK_EQUAL(p.round(t.to_time(7700)), t.to_time(7200));
+    BOOST_CHECK_EQUAL(p.serial(t.to_time(7700)), 12);
+}
+BOOST_AUTO_TEST_SUITE_END()
