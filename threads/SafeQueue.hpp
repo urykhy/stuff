44d4486e3e903944adb4a3346755b1a581c81ede
Yury Stankevich <urykhy@gmail.com>
2018-09-19 21:49:39 +0300

    slow exit for safe queue

--- threads/SafeQueue.hpp
+++ threads/SafeQueue.hpp
@@ -20,41 +20,28 @@ namespace Threads
         void pop()            { m_List.pop_front(); }
     };
 
+    // NOTE: size is not limited
     template<class Node, class Q = ListWrapper<Node>>
     class SafeQueue
     {
-        std::atomic<bool> m_Stop{false};
         mutable std::mutex m_Mutex;
         typedef std::unique_lock<std::mutex> Lock;
-        Q m_List;
+        Q          m_List;
         std::condition_variable m_Cond;
-        unsigned m_WaitCount{0};
+        bool       m_Stop = false;
+        const bool m_FastExit;
 
-        void wakeup_one()
-        {
-            if (m_WaitCount > 0)
-                m_Cond.notify_one();
-        }
-
-        void wakeup_all()
-        {
-            if (m_WaitCount > 0)
-                m_Cond.notify_all();
-        }
-
-        void wait_for(Lock& aLock)
-        {
-            m_WaitCount++;
-            m_Cond.wait_for(aLock, std::chrono::milliseconds(500));
-            m_WaitCount--;
-        }
+        void wakeup_one() { m_Cond.notify_one(); }
+        void wakeup_all() { m_Cond.notify_all(); }
+        void wait_for(Lock& aLock) { m_Cond.wait_for(aLock, std::chrono::milliseconds(500)); }
 
     public:
+        SafeQueue(bool aFastExit = true) : m_FastExit(aFastExit) { }
 
         void stop()
         {
-            m_Stop = true;
             Lock lk(m_Mutex);
+            m_Stop = true;
             wakeup_all();
         }
 
@@ -67,39 +54,40 @@ namespace Threads
 
         bool exiting() const
         {
-            return m_Stop;
+            Lock lk(m_Mutex);
+            return m_FastExit ? (bool)m_Stop : (m_Stop and m_List.empty());
         }
 
-        bool idle(unsigned aExpected) const
+        bool idle() const
         {
             Lock lk(m_Mutex);
-            assert (m_WaitCount <= aExpected);
-            return m_List.empty() && m_WaitCount == aExpected;
+            return m_List.empty();
         }
 
         bool wait(Node& aItem, std::function<bool(Node&)> aTest = [](Node&) -> bool { return true; })
         {
-            while (!m_Stop)
+            Lock lk(m_Mutex);
+
+            if (m_List.empty())
+                wait_for(lk);
+
+            if (!m_List.empty())
             {
-                Lock lk(m_Mutex);
-                if (m_List.empty())
-                    wait_for(lk);
-                if (!m_Stop and !m_List.empty())
+                auto sItem = m_List.top();
+                if (aTest(sItem))
                 {
-                    auto sItem = m_List.top();
-                    if (aTest(sItem))
-                    {
-                        m_List.pop();
-                        if (!m_List.empty())
-                            wakeup_one();   // cond var can miss wakeups, so we try to wakeup next thread from here
-                        lk.unlock();
-                        aItem = std::move(sItem);
-                        return true;
-                    } else {
-                        wait_for(lk);
-                    }
+                    m_List.pop();
+                    if (!m_List.empty())
+                        wakeup_one();   // cond var can miss wakeups, so we try to wakeup next thread from here
+                    lk.unlock();
+                    aItem = std::move(sItem);
+                    return true;
+                } else {
+                    // have some data, but we can't consume it yet. so pause a bit
+                    wait_for(lk);
                 }
             }
+
             return false;
         }
     };
@@ -110,8 +98,8 @@ namespace Threads
         const std::function<void(T& t)> m_Handler;
         SafeQueue<T, Q> m_Queue;
     public:
-        template<class F> SafeQueueThread(F aHandler)
-        : m_Handler(aHandler)  { }
+        template<class F> SafeQueueThread(F aHandler, bool aFastExit = true)
+        : m_Handler(aHandler), m_Queue(aFastExit) { }
 
         void start(Group& aGroup, std::function<bool(T&)> aCheck = [](T&) -> bool { return true; }, unsigned count = 1)
         {
@@ -127,7 +115,7 @@ namespace Threads
         }
 
         void insert(const T& aItem) { m_Queue.insert(aItem); }
-        bool idle() const           { return m_Queue.idle(1); }
+        bool idle() const           { return m_Queue.idle(); }
     };
 
     template<class T>
