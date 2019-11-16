08197ec1156910305c1fb20cd1cc3b0d991a7101
Yury Stankevich <urykhy@gmail.com>
2019-11-16 15:18:47 +0300

    sentry: send message

---
+++ sentry/Client.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <curl/Curl.hpp>
+
+namespace Sentry
+{
+    struct Client
+    {
+        struct Params {
+            std::string url;
+            std::string key;
+            std::string secret;
+            std::string client = "sentry++/1";
+        };
+    private:
+
+        const Params m_Sentry;
+        Curl::Client::Params m_Params;
+        Curl::Client m_Client;
+    public:
+
+        Client(const Params& aSentry)
+        : m_Sentry(aSentry)
+        , m_Client(m_Params)
+        {
+            using Header = Curl::Client::Params::Header;
+            m_Params.headers.emplace_back(Header{"Content-Type","application/json"});
+            m_Params.headers.emplace_back(Header{"X-Sentry-Auth",std::string("Sentry ")
+                + "sentry_key="    + m_Sentry.key + ", "
+                + "sentry_secret=" + m_Sentry.secret + ", "
+                + "sentry_client=" + m_Sentry.client + ", "
+                + "sentry_version=7"
+            });
+        }
+
+        Curl::Client::Result send(const Message& aMsg)
+        {
+            return m_Client.POST(m_Sentry.url, aMsg.to_string());
+        }
+    };
+
+} // namespace Sentry
--- sentry/test.cpp
+++ sentry/test.cpp
@@ -3,6 +3,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include "Message.hpp"
+#include "Client.hpp"
 
 Util::Stacktrace bar() { return Util::GetStacktrace(); }
 Util::Stacktrace foo() { return bar(); }
@@ -17,5 +18,25 @@ BOOST_AUTO_TEST_CASE(simple)
 
     const auto sBT = foo();
     BOOST_TEST_MESSAGE(Sentry::Message().set_exception("ValueError", "44").set_trace(sBT).to_string());
+}
+BOOST_AUTO_TEST_CASE(client)
+{
+    Sentry::Client::Params sParams;
+    sParams.url="web.sentry.docker:9000/api/2/store/";
+    sParams.key="626d891753d6489ba426baa41d7c79fc";
+    sParams.secret="350776c0cfba4013a93275e9de63ba5d";
+    Sentry::Client sClient(sParams);
+
+    const auto sTrace = foo();
+    Sentry::Message sMsg("main");
+    sMsg.set_message("fail to create a shell");
+    sMsg.set_exception("File not found", "/bin/bash");
+    sMsg.set_trace(sTrace);
+    sMsg.set_level("fatal");
+    sMsg.set_environment("test");
+
+    auto rc = sClient.send(sMsg);
+    BOOST_CHECK_EQUAL(rc.first, 200);
+    BOOST_TEST_MESSAGE(rc.second);
 }
 BOOST_AUTO_TEST_SUITE_END()
