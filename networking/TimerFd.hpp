04a578352215116e8cd2436c6790ab3a80b819cd
Yury Stankevich <urykhy@gmail.com>
2019-12-19 21:58:14 +0300

    net: not periodic timer

--- networking/TimerFd.hpp
+++ networking/TimerFd.hpp
@@ -13,14 +13,15 @@ namespace Util
     public:
 
         using Error = Exception::ErrnoError;
-        TimerFd(uint64_t aTimeoutMs = 10)
+        TimerFd(uint64_t aTimeoutMs = 10, bool aMulti = true)
         {
             m_Fd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC);
             if (m_Fd == -1) { throw Error("fail to create timerfd socket"); }
 
             itimerspec sNew;
             memset(&sNew, 0, sizeof(sNew));
-            sNew.it_interval.tv_nsec = aTimeoutMs * 1000 * 1000;
+            if (aMulti)
+                sNew.it_interval.tv_nsec = aTimeoutMs * 1000 * 1000;
             sNew.it_value.tv_nsec = aTimeoutMs * 1000 * 1000;
             if (timerfd_settime(m_Fd, 0, &sNew, nullptr) < 0) { throw Error("fail to setup timer"); }
         }
