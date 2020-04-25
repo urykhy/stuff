260ab4d684f64bb5d60b419fc6305eda7082fe47
Yury Stankevich <urykhy@gmail.com>
2020-04-25 10:40:11 +0300

    unsorted: perform process call

--- unsorted/Process.hpp
+++ unsorted/Process.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <boost/asio.hpp>
 #include <boost/process.hpp>
 
 namespace Util
@@ -12,5 +13,32 @@ namespace Util
         if (!sChild.running())
             throw std::runtime_error("fail to spawn, exit code: " + std::to_string(sChild.exit_code()));
         return sChild;
+    }
+
+    struct XResult
+    {
+        int code = 0;
+        std::string out;
+        std::string err;
+    };
+
+    template<class... T>
+    XResult Perform(const std::string& aCmd, T&&... t)
+    {
+        namespace bp = boost::process;
+        std::future<std::string> out;
+        std::future<std::string> err;
+
+        boost::asio::io_service sAsio;
+        bp::child sChild = Spawn(bp::search_path(aCmd)
+                               , bp::std_in.close()
+                               , bp::std_out > out
+                               , bp::std_err > err
+                               , sAsio
+                               , t...);
+        sAsio.run();
+        sChild.wait();
+
+        return XResult{sChild.exit_code(), out.get(), err.get()};
     }
 }
