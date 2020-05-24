35fcc6174d5989727c5d7bc4a4a78ab52c685d3c
Yury Stankevich <urykhy@gmail.com>
2020-05-24 20:09:53 +0300

    bit of clang format

--- networking/TcpSocket.hpp
+++ networking/TcpSocket.hpp
@@ -1,31 +1,37 @@
 #pragma once
 
-#include <netinet/tcp.h>
 #include "CoreSocket.hpp"
 
-namespace Tcp
-{
+#include <netinet/tcp.h>
+
+namespace Tcp {
     class Socket : public Util::CoreSocket
     {
         void create()
         {
             this->m_Fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-            if (this->m_Fd == -1) { throw Error("fail to create a socket"); }
+            if (this->m_Fd == -1) {
+                throw Error("fail to create a socket");
+            }
         }
-    public:
 
+    public:
         Socket() { create(); }
         explicit Socket(int aFd) { m_Fd = aFd; }
-        Socket(Socket&& aSocket) { m_Fd = aSocket.m_Fd; aSocket.m_Fd = -1; }
+        Socket(Socket&& aSocket)
+        {
+            m_Fd = aSocket.m_Fd;
+            aSocket.m_Fd = -1;
+        }
 
         void connect(uint32_t aRemote, uint16_t aPort)
         {
             struct sockaddr_in sPeer;
             memset(&sPeer, 0, sizeof(sPeer));
             sPeer.sin_family = AF_INET;
-	        sPeer.sin_port = htons(aPort);
-	        sPeer.sin_addr.s_addr = aRemote;
-            if(::connect(m_Fd, (sockaddr*)&sPeer, sizeof(sPeer)) < 0 && errno != EINPROGRESS)
+            sPeer.sin_port = htons(aPort);
+            sPeer.sin_addr.s_addr = aRemote;
+            if (::connect(m_Fd, (sockaddr*)&sPeer, sizeof(sPeer)) < 0 && errno != EINPROGRESS)
                 throw Error("fail to connect");
         }
 
@@ -45,12 +51,12 @@ namespace Tcp
 
         ssize_t read(void* aPtr, ssize_t aSize)
         {
-            return checkCall([&](){ return ::recv(m_Fd, aPtr, aSize, 0); }, "recv");
+            return checkCall([&]() { return ::recv(m_Fd, aPtr, aSize, 0); }, "recv");
         }
 
         ssize_t write(const void* aPtr, ssize_t aSize)
         {
-            return checkCall([&](){ return ::send(m_Fd, aPtr, aSize, 0); }, "send");
+            return checkCall([&]() { return ::send(m_Fd, aPtr, aSize, 0); }, "send");
         }
 
         void set_quick_ack()
@@ -66,7 +72,7 @@ namespace Tcp
                 throw Error("fail to set cork");
         }
 
-        void set_nodelay(int aVal =1)
+        void set_nodelay(int aVal = 1)
         {
             if (setsockopt(m_Fd, IPPROTO_TCP, TCP_NODELAY, &aVal, sizeof(aVal)))
                 throw Error("fail to set nodelay");
@@ -88,11 +94,10 @@ namespace Tcp
 
         void close() override
         {
-            if (m_Fd != -1)
-            {
+            if (m_Fd != -1) {
                 shutdown(m_Fd, SHUT_RDWR);
                 Util::CoreSocket::close();
             }
         }
     };
-}
+} // namespace Tcp
