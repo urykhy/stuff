54bab12d172201714dd315c83723188073b6b0ee
Yury Stankevich <urykhy@gmail.com>
2020-07-30 22:10:21 +0300

    file: fix get_extension

--- file/Util.hpp
+++ file/Util.hpp
@@ -21,10 +21,14 @@ namespace File
     inline std::string get_extension(const std::string& aFilename, bool aSkipTmp = true)
     {
         size_t found = aFilename.find_last_of(".");
+        if (found == std::string::npos)
+            return "";
         std::string sExt = aFilename.substr(found + 1);
         if (aSkipTmp and sExt.size() == 10 and 0 == sExt.compare(0, 4, "tmp-")) // is this File::Tmp extension ?
         {
             auto second = aFilename.rfind(".", found - 1);
+            if (second == std::string::npos)
+                return "";
             sExt = aFilename.substr(second + 1, found - second - 1);
         }
         return sExt;
