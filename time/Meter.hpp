c67f87d81559d92d2b781b6e46bfd41e00362232
Yury Stankevich <urykhy@gmail.com>
2020-09-18 21:47:26 +0300

    time: deadline meter

--- time/Meter.hpp
+++ time/Meter.hpp
@@ -1,34 +1,38 @@
 #pragma once
 #include <stddef.h>
 #include <sys/time.h>
-#include <cmath>
-#include <chrono>
 
-namespace Time
-{
+#include <chrono>
+#include <cmath>
+
+namespace Time {
     struct time_val : public ::timeval
     {
         time_val(const ::timeval& t)
         {
-            tv_sec = t.tv_sec;
+            tv_sec  = t.tv_sec;
             tv_usec = t.tv_usec;
         }
-        time_val(time_t t = 0, suseconds_t us = 0) {
-            tv_sec = t;
+        time_val(time_t t = 0, suseconds_t us = 0)
+        {
+            tv_sec  = t;
             tv_usec = us;
         }
-        time_val(double t) {
-            tv_sec = ceil(t);
+        time_val(double t)
+        {
+            tv_sec  = floor(t);
             tv_usec = (t - tv_sec) * 1000000;
         }
 
-        static time_val now() {
+        static time_val now()
+        {
             struct time_val n;
             gettimeofday(&n, 0);
             return n;
         }
 
-        bool is_null() const {
+        bool is_null() const
+        {
             return tv_sec == 0 && tv_usec == 0;
         }
         time_val operator+(const time_val& tv) const
@@ -45,23 +49,27 @@ namespace Time
         }
         bool operator<(const time_val& tv) const
         {
-            return tv_sec < tv.tv_sec || (tv_sec == tv.tv_sec && tv_usec < tv.tv_usec);
+            return timercmp(this, &tv, <);
         }
         bool operator>(const time_val& tv) const
         {
-            return tv_sec > tv.tv_sec || (tv_sec == tv.tv_sec && tv_usec > tv.tv_usec);
+            return timercmp(this, &tv, >);
         }
-        uint64_t to_ms() const {
+        uint64_t to_ms() const
+        {
             return uint64_t(tv_sec) * 1000 + tv_usec / 1000;
         }
-        uint64_t to_us() const {
+        uint64_t to_us() const
+        {
             return uint64_t(tv_sec) * 1000000 + tv_usec;
         }
-        double to_double() const {
-            return tv_sec + tv_usec/1000000.0;
+        double to_double() const
+        {
+            return tv_sec + tv_usec / 1000000.0;
         }
     };
-    inline std::ostream& operator<<(std::ostream &out, const time_val &t) {
+    inline std::ostream& operator<<(std::ostream& out, const time_val& t)
+    {
         out << t.to_double();
         return out;
     }
@@ -72,8 +80,11 @@ namespace Time
     class Meter
     {
         const time_val m_Start;
+
     public:
-        Meter() : m_Start(get_time()) { }
+        Meter()
+        : m_Start(get_time())
+        {}
         time_val get() const { return get_time() - m_Start; }
     };
 
@@ -81,12 +92,28 @@ namespace Time
     class XMeter
     {
         std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
-    public:
 
+    public:
         std::chrono::duration<unsigned, std::nano> duration() const
         {
             auto stop = std::chrono::high_resolution_clock::now();
-            return std::chrono::duration_cast<std::chrono::nanoseconds>( stop - start );
+            return std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
         }
     };
-}
+
+    // check if deadline expired
+    class Deadline
+    {
+        const time_val m_Deadline;
+
+    public:
+        Deadline(time_val aDeadline)
+        : m_Deadline(get_time() + aDeadline)
+        {}
+
+        bool expired() const
+        {
+            return get_time() > m_Deadline;
+        }
+    };
+} // namespace Time
--- time/test.cpp
+++ time/test.cpp
@@ -7,8 +7,8 @@
 #define BOOST_TEST_MODULE Suites
 #include <boost/test/unit_test.hpp>
 
-#include <Time.hpp>
 #include <Meter.hpp>
+#include <Time.hpp>
 
 BOOST_AUTO_TEST_SUITE(Time)
 BOOST_AUTO_TEST_CASE(simple)
@@ -16,9 +16,9 @@ BOOST_AUTO_TEST_CASE(simple)
     Time::Zone t(Time::load("Europe/Moscow"));
     BOOST_CHECK_EQUAL(t.parse("1970-01-01 01:02:03", Time::DATETIME), -7077);
     BOOST_CHECK_EQUAL(t.parse("1970-01-01 03:00:00", Time::DATETIME), 0);
-    BOOST_CHECK_EQUAL(t.to_time(-7077), cctz::civil_second(1970,01,01,1,2,3));
-    BOOST_CHECK_EQUAL(t.to_unix(cctz::civil_second(1970,01,01,1,2,3)), -7077);
-    BOOST_CHECK_EQUAL(t.to_date(-7077), cctz::civil_day(1970,01,01));
+    BOOST_CHECK_EQUAL(t.to_time(-7077), cctz::civil_second(1970, 01, 01, 1, 2, 3));
+    BOOST_CHECK_EQUAL(t.to_unix(cctz::civil_second(1970, 01, 01, 1, 2, 3)), -7077);
+    BOOST_CHECK_EQUAL(t.to_date(-7077), cctz::civil_day(1970, 01, 01));
     BOOST_CHECK_EQUAL(t.format(-7077, Time::DATETIME), "1970-01-01 01:02:03");
     BOOST_CHECK_EQUAL(t.format(-7077, Time::ISO), "19700101010203");
     BOOST_CHECK_EQUAL(t.format(t.parse("1970-01-01 01:02:03", Time::DATETIME), Time::DATETIME), "1970-01-01 01:02:03");
@@ -26,7 +26,7 @@ BOOST_AUTO_TEST_CASE(simple)
 }
 BOOST_AUTO_TEST_CASE(period)
 {
-    Time::Zone t(cctz::utc_time_zone());
+    Time::Zone   t(cctz::utc_time_zone());
     Time::Period p(t, 600);
 
     BOOST_CHECK_EQUAL(p.round(t.to_time(7700)), t.to_time(7200));
@@ -37,5 +37,15 @@ BOOST_AUTO_TEST_CASE(meter)
     Time::Meter sMeter;
     sleep(1);
     BOOST_CHECK_CLOSE(sMeter.get().to_double(), 1, 0.1); // 0.1%
+}
+BOOST_AUTO_TEST_CASE(deadline)
+{
+    Time::Meter    sMeter;
+    Time::Deadline sTimer(0.5);
+    while (!sTimer.expired()) {
+        const struct timespec sleep_time{0, 1 * 1000 * 1000};
+        nanosleep(&sleep_time, nullptr);
+    }
+    BOOST_CHECK_CLOSE(sMeter.get().to_double(), 0.5, 1);
 }
 BOOST_AUTO_TEST_SUITE_END()
