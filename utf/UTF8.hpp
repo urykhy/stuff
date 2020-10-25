a7d008a9b56cc8f05eb12d5945916ab986a0dfa9
Yury Stankevich <urykhy@gmail.com>
2020-10-25 12:28:38 +0300

    utf-8: fixes

--- utf/UTF8.hpp
+++ utf/UTF8.hpp
@@ -1,100 +1,227 @@
 #pragma once
 
+#include <array>
+#include <iterator>
+#include <stdexcept>
 #include <string>
+#include <string_view>
 
-namespace UTF8
-{
-    struct BadCharacter {};
+#include <mpl/Mpl.hpp>
 
-    inline int trailSize(char cs)
+namespace UTF8 {
+    struct BadCharacter : std::invalid_argument
     {
-        unsigned char c = (unsigned char)cs;
-        if (c <= 0x7F) return 0;
-        if (c <= 0xBF) throw BadCharacter(); // Continuation byte
-        if (c <= 0xDF) return 1;
-        if (c <= 0xEF) return 2;
-        if (c <= 0xF7) return 3;
+        BadCharacter()
+        : std::invalid_argument("invalid utf-8 character")
+        {}
+    };
+
+    inline int trailSize(uint8_t c)
+    {
+        if (c <= 0x7F)
+            return 0;
+        if (c <= 0xBF)
+            throw BadCharacter(); // Continuation byte
+        if (c <= 0xDF)
+            return 1;
+        if (c <= 0xEF)
+            return 2;
+        if (c <= 0xF7)
+            return 3;
         throw BadCharacter();
     }
 
-    inline void checkTrail(char cs)
+    inline void checkTrail(uint8_t c)
     {
-        unsigned char c = (unsigned char)cs;
         if ((c & 0b11000000) != 0b10000000)
             throw BadCharacter();
     }
 
     class CharDecoder
     {
-        uint32_t m_Char = 0;
-        int m_TrailSize = 0; // trail size for character
-        int m_XSize     = 0; // expected characters
+        const std::string_view m_Str;
 
-        void init(char cs)
-        {
-            m_Char = 0;
-            m_TrailSize = trailSize(cs);
-            m_XSize = m_TrailSize;
-
-            unsigned char c = (unsigned char)cs;
-            if (c <= 0x7F) m_Char = c;
-            else if (c <= 0xDF) m_Char = c & 0x1F;
-            else if (c <= 0xEF) m_Char = c & 0x0F;
-            else if (c <= 0xF7) m_Char = c & 0x07;
-            //std::cout << "init char is " << std::hex << m_Char << " ( from " << (unsigned)(unsigned char)cs << ")" << std::endl;
-        }
-
-        void append(char cs)
-        {
-            checkTrail(cs);
-            unsigned char c = (unsigned char)cs;
-            m_Char <<= 6;
-            m_Char |= c & 0x3F;
-        }
-
-        void check()
-        {
-            //std::cout << "test: collected char is " << std::hex << m_Char << std::endl;
-            // ensure minimal length coding
-            if (m_TrailSize > 0 and m_Char <= 0x7F)
-                throw BadCharacter();
-            if (m_TrailSize > 1 and m_Char <= 0x7FF)
-                throw BadCharacter();
-            if (m_TrailSize > 2 and m_Char <= 0xFFFF)
-                throw BadCharacter();
-            if (m_Char > 0x1FFFFF)
-                throw BadCharacter();
-            // detect UTF-16 surrogates
-            if (m_Char >= 0xD800 and m_Char <= 0xDFFF)
-                throw BadCharacter();
-        }
     public:
+        CharDecoder(std::string_view aStr)
+        : m_Str(aStr)
+        {}
 
-        void push_back(char cs)
+        class Iterator
         {
-            if (m_XSize > 0)
-            {
-                m_XSize--;
-                append(cs);
-                if (m_XSize == 0)
-                    check();
-            } else {
-                init(cs);
-            }
-        }
+            std::string_view m_Str;
+            uint32_t         m_Char      = 0; // unicode character
+            uint8_t          m_TrailSize = 0; // trail size for character
+            uint8_t          m_XSize     = 0; // expected characters
+            bool             m_Eol       = false;
 
-        bool done() const { return m_XSize == 0; }
-        uint32_t sym() const { return m_Char; }
+            void init(uint8_t c)
+            {
+                m_Char      = 0;
+                m_TrailSize = trailSize(c);
+                m_XSize     = m_TrailSize;
+
+                if (c <= 0x7F)
+                    m_Char = c;
+                else if (c <= 0xDF)
+                    m_Char = c & 0x1F;
+                else if (c <= 0xEF)
+                    m_Char = c & 0x0F;
+                else if (c <= 0xF7)
+                    m_Char = c & 0x07;
+            }
+
+            void append(uint8_t c)
+            {
+                checkTrail(c);
+                m_Char <<= 6;
+                m_Char |= c & 0x3F;
+            }
+
+            // basic checks
+            void check()
+            {
+                // ensure minimal length coding
+                if (m_TrailSize > 0 and m_Char <= 0x7F)
+                    throw BadCharacter();
+                if (m_TrailSize > 1 and m_Char <= 0x7FF)
+                    throw BadCharacter();
+                if (m_TrailSize > 2 and m_Char <= 0xFFFF)
+                    throw BadCharacter();
+                if (m_Char > 0x1FFFFF)
+                    throw BadCharacter();
+                // detect UTF-16 surrogates
+                if (m_Char >= 0xD800 and m_Char <= 0xDFFF)
+                    throw BadCharacter();
+            }
+
+            void step()
+            {
+                if (m_Str.empty()) {
+                    m_Char = 0;
+                    m_Eol  = true;
+                    return;
+                }
+                while (!m_Str.empty()) {
+                    const uint8_t sChar = (uint8_t)m_Str[0];
+
+                    if (m_XSize > 0) {
+                        m_XSize--;
+                        append(sChar);
+                    } else {
+                        init(sChar);
+                    }
+                    if (m_XSize == 0) {
+                        check();
+                    }
+                    m_Str.remove_prefix(1);
+                    if (done())
+                        return;
+                }
+                throw BadCharacter();
+            }
+
+            bool done() const { return m_XSize == 0; }
+
+        public:
+            using value_type        = uint32_t;
+            using pointer           = uint32_t*;
+            using reference         = uint32_t&;
+            using iterator_category = std::input_iterator_tag;
+            using difference_type   = uint32_t;
+
+            Iterator(std::string_view aStr)
+            : m_Str(aStr)
+            {
+                step();
+            }
+
+            Iterator& operator++()
+            {
+                step();
+                return *this;
+            }
+
+            value_type operator*() const
+            {
+                return m_Char;
+            }
+
+            bool operator==(const Iterator& aOther) const { return m_Str == aOther.m_Str and m_Char == aOther.m_Char and m_Eol == aOther.m_Eol; }
+            bool operator!=(const Iterator& aOther) const { return !(*this == aOther); }
+        };
+
+        Iterator begin() const
+        {
+            return Iterator(m_Str);
+        }
+        Iterator end() const
+        {
+            return Iterator({});
+        }
     };
 
-    inline uint32_t Validate(const std::string& aStr)
+    // decode one character
+    inline uint32_t Decode(std::string_view aStr)
     {
-        CharDecoder sDecoder;
-        for (const char a : aStr)
-            sDecoder.push_back(a);
-        if (!sDecoder.done())
-            throw BadCharacter();
-        return sDecoder.sym();
+        CharDecoder sDecoder(aStr);
+        auto        sIt = sDecoder.begin();
+        if (sIt == sDecoder.end())
+            throw std::invalid_argument("utf8 decode: string is empty");
+        uint32_t sChar = *sIt;
+        if (++sIt != sDecoder.end())
+            throw std::invalid_argument("utf8 decode: not a single character");
+        return sChar;
     }
-}
 
+    // validate string
+    inline void Validate(std::string_view aStr)
+    {
+        for (auto x : CharDecoder(aStr))
+            (void)x;
+    }
+
+    // string length in code points
+    inline uint32_t Length(std::string_view aStr)
+    {
+        CharDecoder sDecoder(aStr);
+        return std::distance(sDecoder.begin(), sDecoder.end());
+    }
+
+    // ensure characters from Basic Multilingual Plane
+    inline bool IsBMP(std::string_view aStr)
+    {
+        for (auto x : CharDecoder(aStr))
+            if (x > 0xFFFF)
+                return false;
+        return true;
+    }
+
+    // https://en.wikipedia.org/wiki/Plane_(Unicode)
+    struct Range
+    {
+        uint32_t begin = 0;
+        uint32_t end   = 0;
+        bool     test(uint32_t x) const { return x >= begin and x <= end; }
+    };
+
+    inline constexpr Range BasicLatin{0x0000, 0x007F};
+    inline constexpr Range BasicCyr{0x0400, 0x04FF};
+
+    template <class... T>
+    inline bool InRange(std::string_view aStr, T&&... aSet)
+    {
+        for (auto x : CharDecoder(aStr)) {
+            bool sHit = false;
+            Mpl::for_each_argument(
+                [x, &sHit](auto range) {
+                    if (!sHit)
+                        sHit |= range.test(x);
+                },
+                aSet...);
+            if (!sHit)
+                return false;
+        }
+        return true;
+    }
+
+} // namespace UTF8
--- utf/test.cpp
+++ utf/test.cpp
@@ -2,21 +2,20 @@
 #include <boost/test/unit_test.hpp>
 
 #include <iostream>
-#include <UTF8.hpp>
 
-// g++ test.cpp -I. -lboost_system -lboost_unit_test_framework -ggdb -std=c++14 -O3
-// ./a.out -l all -t UTF8
+#include <UTF8.hpp>
 
 BOOST_AUTO_TEST_SUITE(UTF8)
 BOOST_AUTO_TEST_CASE(sym)
 {
-    BOOST_CHECK_EQUAL(0x24, UTF8::Validate("\x24"));
-    BOOST_CHECK_EQUAL(0xA2, UTF8::Validate("\xc2\xa2"));
-    BOOST_CHECK_EQUAL(0x20AC, UTF8::Validate("\xe2\x82\xac"));
-    BOOST_CHECK_EQUAL(0x817BC, UTF8::Validate("\xf2\x81\x9e\xbc"));
+    BOOST_CHECK_EQUAL(0x24, UTF8::Decode("\x24"));
+    BOOST_CHECK_EQUAL(0xA2, UTF8::Decode("\xc2\xa2"));
+    BOOST_CHECK_EQUAL(0x20AC, UTF8::Decode("\xe2\x82\xac"));
+    BOOST_CHECK_EQUAL(0x817BC, UTF8::Decode("\xf2\x81\x9e\xbc"));
 
-    BOOST_CHECK_THROW(UTF8::Validate("\xed\xb2\x80"), UTF8::BadCharacter);  // U+DC80
-    BOOST_CHECK_THROW(UTF8::Validate("\xed\xb3\xbf"), UTF8::BadCharacter);  // U+DCFF
+    BOOST_CHECK_THROW(UTF8::Decode("\xed\xb2\x80"), UTF8::BadCharacter); // U+DC80
+    BOOST_CHECK_THROW(UTF8::Decode("\xed\xb3\xbf"), UTF8::BadCharacter); // U+DCFF
+    BOOST_CHECK_THROW(UTF8::Decode("\x24\x24"), std::invalid_argument); // not a signle character
 }
 BOOST_AUTO_TEST_CASE(simple)
 {
@@ -36,5 +35,29 @@ BOOST_AUTO_TEST_CASE(simple)
 
     // good string
     BOOST_CHECK_NO_THROW(UTF8::Validate("'Строка'"));
+
+    // string length in code points
+    BOOST_CHECK_EQUAL(UTF8::Length("test строка"), 11);
+
+    // allow null characters
+    std::string sTmp("tttt\x00\x00\x00rrrr", 11);
+    BOOST_CHECK_EQUAL(UTF8::Length(sTmp), 11);
+
+    // plane and range
+    BOOST_CHECK_EQUAL(UTF8::IsBMP("тест string"), true);
+    BOOST_CHECK_EQUAL(UTF8::IsBMP("\xf0\x90\x85\x83"), false); // GREEK ACROPHONIC ATTIC FIVE
+
+    BOOST_CHECK_EQUAL(UTF8::InRange("test", UTF8::BasicLatin), true);
+    BOOST_CHECK_EQUAL(UTF8::InRange("тест", UTF8::BasicLatin), false);
+    BOOST_CHECK_EQUAL(UTF8::InRange("тест string", UTF8::BasicCyr, UTF8::BasicLatin), true);
+}
+BOOST_AUTO_TEST_CASE(iterate)
+{
+    std::vector<uint32_t> sExpected{0x0442, 0x0435, 0x0441, 0x0442, 0x0020, 0x0073, 0x0074, 0x0072, 0x0069, 0x006E, 0x0067};
+    std::vector<uint32_t> sResult;
+    UTF8::CharDecoder     sDecoder("тест string");
+    for (auto x : sDecoder)
+        sResult.push_back(x);
+    BOOST_CHECK_EQUAL_COLLECTIONS(sResult.begin(), sResult.end(), sExpected.begin(), sExpected.end());
 }
 BOOST_AUTO_TEST_SUITE_END()
