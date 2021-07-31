b7d149cdf8c5644142b9f91f17bd6910b8e1c58f
Yury Stankevich <urykhy@gmail.com>
2021-07-31 15:28:58 +0300

    swagger: log calls

--- format/Float.hpp
+++ format/Float.hpp
@@ -1,3 +1,4 @@
+#pragma once
 
 #include <cmath>
 #include <iomanip>
@@ -5,17 +6,24 @@
 #include <sstream>
 
 namespace Format {
-    std::string with_precision(double v, int precision)
+    inline std::string with_precision(double v, int precision)
     {
         if (!std::isfinite(v))
             return "0";
         std::stringstream s;
         s.setf(std::ios::fixed, std::ios::floatfield);
         s << std::setprecision(precision) << v;
-        return s.str();
+        std::string tmp = s.str();
+
+        while (tmp.size() > 1 and tmp.back() == '0')
+            tmp.pop_back();
+        if (tmp.size() > 1 and tmp.back() == '.')
+            tmp.pop_back();
+
+        return tmp;
     }
 
-    std::string for_human(double v)
+    inline std::string for_human(double v)
     {
         if (v > 1E10)
             return with_precision(v / 1E9, 0) + "G";
@@ -23,6 +31,14 @@ namespace Format {
             return with_precision(v / 1E6, 0) + "M";
         if (v > 1E4)
             return with_precision(v / 1E3, 0) + "K";
-        return with_precision(v, 0);
+        if (v > 100)
+            return with_precision(v, 0);
+        if (v > 10)
+            return with_precision(v, 1);
+        if (v > 1)
+            return with_precision(v, 2);
+        if (v > 1E-3)
+            return with_precision(v * 1E3, 0) + "m";
+        return with_precision(v * 1E6, 0) + "u";
     }
 } // namespace Format
