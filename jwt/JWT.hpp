4c49d3d09f9bc68a419323c8cc714c38d22cc140
Yury Stankevich <urykhy@gmail.com>
2021-08-21 16:29:34 +0300

    jwt: fixes

--- jwt/JWT.hpp
+++ jwt/JWT.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string_view>
+
 #include <exception/Error.hpp>
 #include <format/Base64.hpp>
 #include <format/Hex.hpp>
@@ -11,13 +13,32 @@
 
 namespace Jwt {
 
+    struct Header
+    {
+        std::string typ = "JWT";
+        std::string alg = {};
+
+        void from_json(const ::Json::Value& aJson)
+        {
+            Parser::Json::from_value(aJson, "typ", typ);
+            Parser::Json::from_value(aJson, "alg", alg);
+        }
+        Format::Json::Value to_json() const
+        {
+            Format::Json::Value sValue(::Json::objectValue);
+            sValue["typ"] = Format::Json::to_value(typ);
+            sValue["alg"] = Format::Json::to_value(alg);
+            return sValue;
+        }
+    };
+
     struct Claim
     {
-        time_t      exp      = {}; // expiration time on and after which the JWT must not be accepted for processing.
-        time_t      nbf      = {}; // time on which the JWT will start to be accepted for processing.
-        std::string iss      = {}; // issuer
-        std::string aud      = {}; // audience
-        std::string username = {};
+        time_t      exp = {}; // expiration time on and after which the JWT must not be accepted for processing.
+        time_t      nbf = {}; // time on which the JWT will start to be accepted for processing.
+        std::string iss = {}; // issuer
+        std::string aud = {}; // audience
+        std::string sub = {}; // subject
 
         void from_json(const ::Json::Value& aJson)
         {
@@ -25,61 +46,51 @@ namespace Jwt {
             Parser::Json::from_value(aJson, "nbf", nbf);
             Parser::Json::from_value(aJson, "iss", iss);
             Parser::Json::from_value(aJson, "aud", aud);
-            Parser::Json::from_value(aJson, "username", username);
+            Parser::Json::from_value(aJson, "sub", sub);
         }
         Format::Json::Value to_json() const
         {
             Format::Json::Value sValue(::Json::objectValue);
-            sValue["exp"]      = Format::Json::to_value(exp);
-            sValue["nbf"]      = Format::Json::to_value(nbf);
-            sValue["iss"]      = Format::Json::to_value(iss);
-            sValue["aud"]      = Format::Json::to_value(aud);
-            sValue["username"] = Format::Json::to_value(username);
+            sValue["exp"] = Format::Json::to_value(exp);
+            sValue["nbf"] = Format::Json::to_value(nbf);
+            sValue["iss"] = Format::Json::to_value(iss);
+            sValue["aud"] = Format::Json::to_value(aud);
+            sValue["sub"] = Format::Json::to_value(sub);
             return sValue;
         }
     };
 
-    class Manager
+    using Error = std::invalid_argument;
+
+    struct IFace
+    {
+        virtual std::string Sign(const Claim& aClaim) const               = 0;
+        virtual Claim       Validate(const std::string_view aToken) const = 0;
+        virtual ~IFace(){};
+    };
+
+    class HS256 : public IFace
     {
         const SSLxx::KeyPtr m_Key;
         static unsigned constexpr URL_SAFE = Format::BASE64_NO_PADDING | Format::BASE64_URL_SAFE;
 
     public:
-        struct Header
-        {
-            std::string typ = "JWT";
-            std::string alg = "HS256";
-
-            void from_json(const ::Json::Value& aJson)
-            {
-                Parser::Json::from_value(aJson, "typ", typ);
-                Parser::Json::from_value(aJson, "alg", alg);
-            }
-            Format::Json::Value to_json() const
-            {
-                Format::Json::Value sValue(::Json::objectValue);
-                sValue["typ"] = Format::Json::to_value(typ);
-                sValue["alg"] = Format::Json::to_value(alg);
-                return sValue;
-            }
-        };
-
-        using Error = Exception::Error<Manager, std::invalid_argument>;
-
-        Manager(const std::string& aKey)
+        HS256(const std::string& aKey)
         : m_Key(SSLxx::hmacKey(aKey))
         {}
 
-        std::string Sign(const Claim& aClaim) const
+        virtual ~HS256() {}
+
+        std::string Sign(const Claim& aClaim) const override
         {
-            std::string sHeader = Format::Base64(Format::Json::to_string(Format::Json::to_value(Header{}), false), URL_SAFE);
+            std::string sHeader = Format::Base64(Format::Json::to_string(Format::Json::to_value(Header{.alg = "HS256"}), false), URL_SAFE);
             std::string sClaim  = Format::Base64(Format::Json::to_string(Format::Json::to_value(aClaim), false), URL_SAFE);
             std::string sHash   = SSLxx::HMAC::Sign(EVP_sha256(), m_Key, sHeader, std::string_view("."), sClaim);
 
             return sHeader + '.' + sClaim + '.' + Format::Base64(sHash, URL_SAFE);
         }
 
-        Claim Validate(const std::string& aToken) const
+        Claim Validate(const std::string_view aToken) const override
         {
             std::vector<std::string_view> sParts;
             Parser::simple(
--- jwt/test.cpp
+++ jwt/test.cpp
@@ -3,44 +3,42 @@
 
 #include "JWT.hpp"
 
-const Jwt::Manager sManager("secret");
+const Jwt::HS256 sManager("secret");
 
 BOOST_AUTO_TEST_SUITE(JWT)
 BOOST_AUTO_TEST_CASE(basic)
 {
-    Jwt::Claim sClaim{.exp      = time(nullptr) + 10,
-                      .nbf      = time(nullptr) - 10,
-                      .iss      = "iss",
-                      .aud      = "aud",
-                      .username = "test"};
+    Jwt::Claim sClaim{.exp = time(nullptr) + 10,
+                      .nbf = time(nullptr) - 10,
+                      .iss = "iss",
+                      .aud = "aud",
+                      .sub = "test"};
     BOOST_TEST_MESSAGE(Format::Json::to_string(Format::Json::to_value(sClaim)));
 
-    const std::string  sToken = sManager.Sign(sClaim);
+    const std::string sToken = sManager.Sign(sClaim);
     BOOST_TEST_MESSAGE("token: " << sToken);
 
     const auto sResult = sManager.Validate(sToken);
     BOOST_CHECK_EQUAL(sClaim.iss, sResult.iss);
     BOOST_CHECK_EQUAL(sClaim.aud, sResult.aud);
-    BOOST_CHECK_EQUAL(sClaim.username, sResult.username);
+    BOOST_CHECK_EQUAL(sClaim.sub, sResult.sub);
 }
 BOOST_AUTO_TEST_CASE(nbf)
 {
-    Jwt::Claim sClaim{.exp      = time(nullptr) + 10,
-                      .nbf      = time(nullptr) + 10, // nbf in future
-                      .iss      = "iss",
-                      .aud      = "aud",
-                      .username = "test"};
-    const std::string  sToken = sManager.Sign(sClaim);
-    BOOST_CHECK_THROW(sManager.Validate(sToken), Jwt::Manager::Error);
+    Jwt::Claim        sClaim{.exp = time(nullptr) + 10,
+                      .nbf = time(nullptr) + 10, // nbf in future
+                      .iss = "iss",
+                      .aud = "aud"};
+    const std::string sToken = sManager.Sign(sClaim);
+    BOOST_CHECK_THROW(sManager.Validate(sToken), Jwt::Error);
 }
 BOOST_AUTO_TEST_CASE(exp)
 {
-    Jwt::Claim sClaim{.exp      = time(nullptr) - 10, // expired in the past
-                      .nbf      = time(nullptr) - 10,
-                      .iss      = "iss",
-                      .aud      = "aud",
-                      .username = "test"};
-    const std::string  sToken = sManager.Sign(sClaim);
-    BOOST_CHECK_THROW(sManager.Validate(sToken), Jwt::Manager::Error);
+    Jwt::Claim        sClaim{.exp = time(nullptr) - 10, // expired in the past
+                      .nbf = time(nullptr) - 10,
+                      .iss = "iss",
+                      .aud = "aud"};
+    const std::string sToken = sManager.Sign(sClaim);
+    BOOST_CHECK_THROW(sManager.Validate(sToken), Jwt::Error);
 }
 BOOST_AUTO_TEST_SUITE_END()
