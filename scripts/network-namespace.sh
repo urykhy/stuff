a9787b990170380c479a39519f1d3eaaa026e6bc
Yury Stankevich <urykhy@gmail.com>
2021-11-26 21:44:56 +0300

    scripts: firewall

--- scripts/network-namespace.sh
+++ scripts/network-namespace.sh
@@ -18,10 +18,11 @@ ip netns exec $NAME ip route add 10.103.0.0/16 via 172.18.1.1 dev space1
 mkdir -p /etc/netns/$NAME
 echo 'nameserver 10.103.10.3' > /etc/netns/$NAME/resolv.conf
 
+ip netns exec $NAME iptables -F
 ip netns exec $NAME iptables -A INPUT -m conntrack --ctstate established,related -j ACCEPT
-ip netns exec $NAME iptables -A INPUT -p tcp -m multiport --dports 6771,6881 -j ACCEPT
-ip netns exec $NAME iptables -A INPUT -p udp -m multiport --dports 6771,6881 -j ACCEPT
 ip netns exec $NAME iptables -A INPUT -j REJECT
+ip netns exec $NAME iptables -A FORWARD -m conntrack --ctstate established,related -j ACCEPT
+ip netns exec $NAME iptables -A FORWARD -j REJECT
 
 # ip netns exec protonvpn /bin/bash
 ip netns exec $NAME /etc/init.d/openvpn start proton
