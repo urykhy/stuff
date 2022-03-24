5da938dcf9026ee212d9737a0024a50e452d4873
Yury Stankevich <urykhy@gmail.com>
2022-03-24 21:06:00 +0300

    swagger: arrange files

--- swagger/get_swagger_ui.sh
+++ swagger/get_swagger_ui.sh
@@ -6,8 +6,8 @@ ROOT="https://raw.githubusercontent.com/swagger-api/swagger-ui/master/dist"
 mkdir tmp_ui
 pushd tmp_ui
 wget $ROOT/{swagger-ui.css,swagger-ui-bundle.js,swagger-ui-standalone-preset.js}
-cp ../../*.yaml .
-cp ../../index.html .
+cp ../../api/*.yaml .
+cp ../../api/index.html .
 tar -cvf ../swagger_ui.tar *
 popd
 rm -rf ./tmp_ui
